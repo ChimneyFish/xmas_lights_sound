76170bb6aea76f458f80b0d7a5560ec1a787dfdb
ChimneyFish <115329981+ChimneyFish@users.noreply.github.com>
2023-12-05 13:51:19 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ song_move="$DIR"/data/randomp3.sh
 #
 sudo apt update && sudo apt install openjdk-17-jdk alsa* python3 -y
 chmod +x "$DIR"/data/randomp3.sh
-wget https://github.com/processing/processing4/releases/download/processing-1293-4.3/processing-4.3-linux-x64.tgz && tar -xf processing-1293-4.3/processing-4.3-linux-x64.tgz && 
+wget https://github.com/processing/processing4/releases/download/processing-1293-4.3/processing-4.3-linux-arm64.tgz && tar -xf processing-4.3-linux-arm64.tgz 
 $INTP
 #
 read -p "Do you want to pull all mp3s from a single folder? Press 1 for yes, press any other key to exit and place MP3s manually: " N
 #
-if [ "$N" -eq 1 ]; then
-    "$DIR"/data/randomp3.sh
+if [ "$N" -eq 1 ]; do
+   sh "$DIR"/data/randomp3.sh
 else
     exit
 fi
