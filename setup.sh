67682a4dd7d6d4a2e809a5d82b388744cc944b4b
ChimneyFish <115329981+ChimneyFish@users.noreply.github.com>
2023-12-05 13:43:38 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ $INTP
 read -p "Do you want to pull all mp3s from a single folder? Press 1 for yes, press any other key to exit and place MP3s manually: " N
 
 if [ "$N" -eq 1 ]; then
-    ."$DIR"/data/randomp3.sh
+    "$DIR"/data/randomp3.sh
 else
     exit
 fi
